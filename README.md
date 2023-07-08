e0e159ad85f1814625dbf834241d570a38daa5f4
sSsA01 <kawashima.satoshi.43m@kyoto-u.jp>
2023-07-08 16:00:29 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# competitiveProgramming_library
+# competitive_programming_library
+## What's this?
+競技プログラミング向けにVisual Studioのproject templateを整備したい
+
+## How to
+### How to generate a project template by this repository
+1. 以下のファイル群をzip圧縮
+	* templateフォルダ内の全ファイル
+	* library\competitive_programming_templateの下記ファイル
+		* competitive_programming_template.vcxproj.user
+		* main.cpp
+1. 適切なフォルダへ設置
+	* 例
+		* C:\Users\USER_NAME\Documents\Visual Studio 2022\Templates\ProjectTemplates
+1. 新規プロジェクト作成時にテンプレートとして使用。
+
+## Reference
+### How to: Create project templates
+https://learn.microsoft.com/ja-jp/visualstudio/ide/how-to-create-project-templates?view=vs-2022
+### プロジェクトと同名のファイルを生成したい
+https://learn.microsoft.com/en-us/visualstudio/extensibility/projectitem-element-visual-studio-project-templates?view=vs-2022
---
+++ library/competitive_programming_template/main.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <cmath>
+#include <string>
+#include <optional>
+#include <vector>
+#include <set>
+#include <numeric>
+#include <limits>
+
+namespace sss
+{
+	namespace io
+	{
+		auto& outStream = std::cout;
+
+		constexpr auto* const SPACE		= " ";
+		constexpr auto* const NEW_LINE	= "\n";
+
+		std::string  TRUE_TEXT = "Yes";
+		std::string FALSE_TEXT = "No";
+
+		template<class T>
+		void OutputData(const T& value)
+		{
+			outStream << value;
+		}
+
+		void OutputData(const bool is)
+		{
+			outStream << (is ? TRUE_TEXT : FALSE_TEXT);
+		}
+
+		template<class T>
+		void OutputLine(const T& value)
+		{
+			OutputData(value);
+			OutputData(NEW_LINE);
+		}
+
+		template<class T>
+		void OutputLine(const std::vector<T>& collection)
+		{
+			if (collection.empty()) {
+				return;
+			}
+
+			OutputData(collection.front());
+
+			std::for_each(
+				std::next(std::cbegin(collection)), std::cend(collection),
+				[&](const auto& value)
+				{
+					OutputData(SPACE);
+					OutputData(value);
+				}
+			);
+
+			OutputData(NEW_LINE);
+		}
+
+		// 可変引数テンプレート
+		// https://cpprefjp.github.io/lang/cpp11/variadic_templates.html
+		//template<class Head, class... Tail>
+		//void Output(const std::vector<Head>& head, const std::vector<Tail>&... tail)
+		//{
+		//	OutputLine(head);
+		//	Output(tail...);
+		//}
+
+	} // namespace io
+
+} // namespace sss
+
+class Solver
+{
+public:
+	static void Run()
+	{
+		InputData input{};
+		Input(input);
+
+		Solution solution{};
+		Solve(input, solution);
+
+		Output(solution);
+	}
+
+private:
+	struct InputData
+	{
+		// mock
+		int mock;
+	};
+
+	struct Solution
+	{
+		// mock
+		int		mock;
+		bool	isMock;
+		std::vector<size_t> mocks;
+	};
+
+	static void Input(InputData& input)
+	{
+		// mock
+		std::cin >> input.mock;
+	}
+
+	static void Solve(InputData& input, Solution& solution)
+	{
+	}
+
+	static void Output(const Solution& solution)
+	{
+		// mock
+		sss::io::OutputLine(solution.mock);
+		sss::io::OutputLine(solution.isMock);
+		sss::io::OutputLine(solution.mocks);
+	}
+};
+
+int main()
+{
+	Solver::Run();
+}
